24813f79e069594af6c08d368c8d2c3beadf2c3e
niki-01 <niki.manoledaki@gmail.com>
2020-01-02 14:51:49 +0100

    Calculates the score

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,23 +4,30 @@ require_relative 'deck.rb'
 class Blackjack
     attr_reader :player, :player_hand, :dealer, :dealer_hand, :deck
 
-    def initialize(dealer = Player.new)
-      @player = nil
+    def initialize(dealer = Player.new, player = Player.new, deck = Deck.new)
+      @player = player
       @player_hand = []
       @dealer = dealer
       @dealer_hand = []
-      @deck = Deck.new
+      @deck = deck
+      @turn
     end 
-
-    def player(name = Player.new)
-      @name = name
-    end
 
     def start
       2.times do 
-        @player_hand << @deck.random_card
-        @dealer_hand << @deck.random_card
+        @player_hand << draw
+        @dealer_hand << draw
       end
+    end
+
+    def draw
+      @deck.random_card
+    end
+
+    def score(hand)
+      score = 0
+      hand.each {|x| score += deck.value(x) }
+      score
     end
     
 end
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -1,25 +1,24 @@
 require 'blackjack'
 
 describe Blackjack do
-  describe '#player' do
-    it 'initializes a player' do
-      sam = "Sam"
-      expect(subject.player(sam)).to eq "Sam"
-    end
-  end
 
   describe '#start' do
     it 'gives two cards from the deck to each' do
-      sam = "Sam"
-      subject.player(sam)
       expect{ subject.start }.to change{ subject.player_hand }
     end
   end
 
-#   describe '#draw' do
-#     it 'adds a card to hand' do
-#       expect{ subject.draw }.to change{ subject.player_hand }
-#     end
-#   end
+  describe '#draw' do
+    it 'adds a card to hand' do
+      expect(subject.draw).to be_a(String)
+    end
+  end
+
+  describe '#score' do
+    it 'calculates the score' do
+        hand = ["four", "five"]
+        expect(subject.score(hand)).to eq 9
+    end
+  end
 
 end
